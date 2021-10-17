63a4fe506df68871d4a0619a6392ef4d2bdfd78b
jaomaloy <jaomaloy@protonmail.com>
2021-10-17 21:30:03 +0800

    Git support in packer

--- init.vim
+++ init.vim
@@ -1,4 +1,5 @@
 lua require'plugins'
+lua require'gitsigns'
 source $HOME/.config/nvim/settings.vim
 source $HOME/.config/nvim/keybindings.vim
 source $HOME/.config/nvim/themes/onedark.vim
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,8 +8,12 @@ return require('packer').startup(function()
       use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
       use 'navarasu/onedark.nvim'
 
-    use 'jiangmiao/auto-pairs'
+    -- Git support
     use 'tpope/vim-fugitive'
+    use {
+        'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+        config = function() require('gitsigns').setup() end
+    }
     use 'tpope/vim-obsession'
     -- use 'tpope/vim-repeat'
     -- use 'tpope/vim-eunuch'
